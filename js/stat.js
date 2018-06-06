926816aee6e2e7524b71a705aad480f84736fc97
Денис Кузнецов <lifeditor@mail.ru>
2018-06-06 11:56:43 +0500

    Добавил stat.js для вывода окна с результатами игроков

--- js/stat.js
+++ js/stat.js
@@ -19,6 +19,9 @@ var BAR_OFFSET_X = 55;
 var PLAYER_NAME = 'Вы';
 var PLAYER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
 
+var ALPHA_MIN = 0.1;
+var ALPHA_MAX = 1;
+
 // параметры текста
 var TEXT_COLOR = '#000';
 var FONT_SIZE = 16;
@@ -26,10 +29,13 @@ var TEXT_LINE1 = 'Ура вы победили!';
 var TEXT_LINE2 = 'Список результатов:';
 var fontStyle = FONT_SIZE + 'px "PT Mono"';
 
-var alphaValues = ['0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1'];
+var barX = CLOUD_X + BAR_OFFSET_X;
 var barY = CLOUD_Y + CLOUD_HEIGHT - FONT_SIZE * 2;
+var barTitleY = CLOUD_Y + CLOUD_HEIGHT - FONT_SIZE;
 var messageX = CLOUD_X + FONT_SIZE;
 var messageY = CLOUD_Y + FONT_SIZE * 2;
+var min = ALPHA_MIN * 10;
+var max = ALPHA_MAX * 10;
 
 var getMaxElement = function (times) {
   var maxElement = times[0];
@@ -42,12 +48,25 @@ var getMaxElement = function (times) {
 };
 
 var generateColor = function () {
-  return 'rgba(0, 0, 120, ' + alphaValues[Math.floor(Math.random() * alphaValues.length)] + ')';
+  // значения альфа 0.1 .. 0.9 при ALPHA_MIN = 0.1 и ALPHA_MAX = 1
+  return 'rgba(0, 0, 120, 0.' + (Math.floor(Math.random() * (max - min)) + min) + ')';
 };
 
 var fillCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var fillBar = function (ctx, name, time, x, dY) {
+  ctx.fillStyle = (name === PLAYER_NAME) ? PLAYER_BAR_COLOR : generateColor();
+  ctx.fillRect(x, barY, BAR_WIDTH, -dY);
+  fillTitles(ctx, name, time, x, barTitleY, barY - dY - FONT_SIZE / 2);
+};
+
+var fillTitles = function (ctx, text1, text2, x, y1, y2) {
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.fillText(text1, x, y1);
+  ctx.fillText(text2, x, y2);
 };
 
 window.renderStatistics = function (ctx, names, times) {
@@ -60,16 +79,9 @@ window.renderStatistics = function (ctx, names, times) {
   fillCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
   for (var i = 0; i < names.length; i++) {
-    var barX = CLOUD_X + BAR_OFFSET_X + (BAR_WIDTH + BAR_GAP) * i;
-    var barDeltaY = Math.floor(BAR_HEIGHT * times[i] / maxTime);
-    ctx.fillStyle = (names[i] === PLAYER_NAME) ? PLAYER_BAR_COLOR : generateColor();
-    ctx.fillRect(barX, barY, BAR_WIDTH, -barDeltaY);
-    ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(names[i], barX, barY + FONT_SIZE);
-    ctx.fillText(Math.floor(times[i]), barX, barY - barDeltaY - FONT_SIZE / 2);
+    var dY = Math.floor(BAR_HEIGHT * times[i] / maxTime);
+    fillBar(ctx, names[i], Math.floor(times[i]), barX + (BAR_WIDTH + BAR_GAP) * i, dY);
   }
 
-  ctx.fillText(TEXT_LINE1, messageX, messageY);
-  ctx.fillText(TEXT_LINE2, messageX, messageY + FONT_SIZE);
-
+  fillTitles(ctx, TEXT_LINE1, TEXT_LINE2, messageX, messageY, messageY + FONT_SIZE);
 };
